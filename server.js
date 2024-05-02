dc0ece8fe3910958e8a85e2d8bf7017a1c048334
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-03 00:11:52 +0200

    feat: :sparkles: nodemon added, /api path removed

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-//backend/index.js
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const cors = require('cors')
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+const postRoutes = require('./routes/postRoutes')
 
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postRoutes');
-
-dotenv.config();
-const app = express();
-app.use(helmet());  // for some basic security best practices
-app.use(cors());    // Enable CORS
-app.use(express.json());
+dotenv.config()
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
 // Define a simple root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the StudentConnect API!');
-});
+  res.send('Welcome to the StudentConnect API!')
+})
 
 // Use routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
