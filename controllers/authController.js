fcee98b2a04c4a44496e661db9352ad1fbdd91ec
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-03 03:10:38 +0200

    temporarily return user upon authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,50 +1,53 @@
 //backend/controllers/authController.js
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/users');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/users')
 
 exports.register = async (req, res) => {
-  const { username, email, phoneNumber, password, role } = req.body; // include 'role' to specify user or admin
+  const { username, email, phoneNumber, password, role } = req.body // include 'role' to specify user or admin
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
     const newUser = new User({
       username,
       email,
       phoneNumber,
       hashedPassword,
-      role
-    });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
+      role,
+    })
+    await newUser.save()
+    res
+      .status(201)
+      .json({ message: 'User registered successfully', user: newUser })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
-};
-
+}
 
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  console.log('Attempting login with', email, password); // Debugging output
+  const { email, password } = req.body
+  console.log('Attempting login with', email, password) // Debugging output
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
     if (!user) {
-      console.log('User not found for email:', email); // Debugging output
-      return res.status(404).send('User not found');
+      console.log('User not found for email:', email) // Debugging output
+      return res.status(404).send('User not found')
     }
 
-    const isMatch = await bcrypt.compare(password, user.hashedPassword);
+    const isMatch = await bcrypt.compare(password, user.hashedPassword)
     if (!isMatch) {
-      console.log('Password does not match for user:', email); // Debugging output
-      return res.status(400).send('Invalid credentials');
+      console.log('Password does not match for user:', email) // Debugging output
+      return res.status(400).send('Invalid credentials')
     }
 
-    const token = jwt.sign({ userID: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ message: 'Login successful', token });
+    const token = jwt.sign(
+      { userID: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    )
+    res.json({ message: 'Login successful', token, user: user })
   } catch (error) {
-    console.error('Login error for user:', email, error); // Debugging output
-    res.status(500).send(error.message);
+    console.error('Login error for user:', email, error) // Debugging output
+    res.status(500).send(error.message)
   }
-};
-
-
+}
