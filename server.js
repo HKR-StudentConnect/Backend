820a1d64ff8a3f269c8676373e45a6b6b89f497f
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-21 00:19:54 +0200

    fix: :bug: Fixes issue with express server

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const http = require('http')
-const { Server } = require('socket.io')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
@@ -9,15 +7,12 @@ const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
 const postRoutes = require('./routes/postRoutes')
 const chatRoutes = require('./routes/chatRoutes')
-const User = require('./models/users')
 const adminRoutes = require('./routes/adminRoutes')
 const notificationRoutes = require('./routes/notificationsRoutes')
 const eventRoutes = require('./routes/eventRoutes')
 
 dotenv.config()
 const app = express()
-// const server = http.createServer(app)
-// const io = new Server(server)
 
 app.use(helmet())
 app.use(cors())
@@ -32,40 +27,15 @@ app.use('/admin', adminRoutes)
 app.use('/notifications', notificationRoutes)
 app.use('/events', eventRoutes)
 
-// Socket.io for real-time chat
-// io.on('connection', socket => {
-//   console.log('User connected:', socket.id)
-
-//   socket.on('go-online', userId => {
-//     socket.join(userId)
-//     User.findByIdAndUpdate(userId, { onlineStatus: true }, err => {
-//       if (err) console.log('Error setting user online:', err)
-//       io.emit('user-online', userId)
-//     })
-//   })
-
-//   socket.on('send-message', message => {
-//     const { senderId, receiverId, text } = message
-//     io.to(receiverId).emit('receive-message', { senderId, text })
-//     // saving the message here via chatController
-//   })
-
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id)
-//     // Optionally, set user offline in the database
-//   })
-// })
-
 app.get('/', (req, res) => {
   res.send('Welcome to StudentConnect!!')
 })
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected')
-    server.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    )
-  })
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}.`)
+})
